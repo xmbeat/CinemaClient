dbdebd440a58c8950ff8e93f676f3d86ce8ff27a
xmbeat <xmbeat@hotmail.com>
2014-12-10 21:10:09 -0600

    Ahora si??

--- unittest/mock_movieclient.py
+++ unittest/mock_movieclient.py
@@ -5,7 +5,7 @@ sys.path.append("../src/")
 from movie_client import MovieClient
 class MockTest(unittest.TestCase):
     
-    @mock.patch('src.movie_client.MovieClient')
+    @mock.patch('movie_client.MovieClient')
     def test_updateActor_21(self, mock_cliente ):
         mock_cliente.updateActor.return_value = True
         val = mock_cliente.updateActor({"idActor":"21", "nombre": "Juan Hebert"})
@@ -18,7 +18,7 @@ class MockTest(unittest.TestCase):
         pelicula = cliente.obtenPelicula(22)
         self.assertEqual(31, pelicula.reparto[0]["idActor"])
         
-    @mock.patch('src.movie_client.MovieClient.getJSON')    
+    @mock.patch('movie_client.MovieClient.getJSON')    
     def test_obtenActor_1231(self, mock_read):
         cliente = MovieClient()
         mock_read.return_value = '{"id": "1", "place_of_birth" : "Veracruz","name" : "Juan Hebert", "homepage" : "", "birthday" : "1992-11-12", "deathday":"", "profile_path": ""}'
